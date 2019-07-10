ff52fb55278bb3c2915414cf59acce08aaef6c2c
aloneThinker <1186054130@qq.com>
2019-07-10 21:49:03 +0800

    update pojo

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -2,5 +2,13 @@ package com.itheima.pojo;
 
 public class User {
 private String name;
+private Integer age;
+private String sex;
+
+    public User(String name, Integer age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
 }
